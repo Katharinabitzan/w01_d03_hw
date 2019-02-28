e6efc51e81053f3e15f7aece37f34bc26d48f25d
Katharina Bitzan <katharinabitzan@Katharinas-MacBook-Pro-3.local>
2019-02-28 10:03:50 +0000

    added homework review exc. A

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -2,47 +2,57 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 # 1. Add `"Edinburgh Waverley"` to the end of the array
 stops.push("Edinburgh Waverley")
-#puts stops
+##stops << "Edinburgh Waverley"
 
 # 2. Add `"Glasgow Queen St"` to the start of the array
 stops.unshift("Glasgow Queen St")
-#puts stops
+##stops.insert(0, "Glasgow Queen St")
+##["Glasgow Queen St"] += stops     -> kind of a long way to do it
 
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
-stops.insert(4, "Polmont")
-#puts stops
+##stops.insert(4, "Polmont")
+##stops.insert(stops.index("Linlithgow"), "Polmont")
+index_of_falkirk_high = stops.index("Falkirk High") + 1
+stops.insert(index_of_falkirk_high, "Polmont")
 
 # 4. Work out the index position of `"Linlithgow"`
 position = stops.index("Linlithgow")
-#p position  # position = 5
+##counter = 0
+##index = 0
+##while (counter < stops.size())
+  ##if (stops[counter] =="Linlithgow")
+  ##index = counter
+  ##break
+  ##end
+##end
 
 # 5. Remove `"Livingston"` from the array using its name
 stops.delete("Livingston")
-#puts stops
 
 # 6. Delete `"Cumbernauld"` from the array by index
 stops.delete(stops[2])
-#puts stops
+##stops.delete_at(2)
 
 # 7. How many stops there are in the array?
 number_of_stops = stops.count
-#p number_of_stops  # number_of_stops = 5
+## number_of_stops = stops.index(stops.last) + 1
+## number_of_stops = stops.length()
+## number_of_stops = stops.size()
 
 # 8. How many ways can we return `"Falkirk High"` from the array?
-#p stops[2]
-#p stops[-5]
-#p stops[2, 1]
-#p stops.slice(2, 1)
+stops[2]
+stops[-5]
+stops[2, 1]
+stops.slice(2, 1)
+## stops.at(2)
+## stops.fetch(2)
+## stops.take(2)
 
 # 9. Reverse the positions of the stops in the array
 reversed = stops.reverse
-puts reversed
-
 stops.reverse!
-#puts stops
 
 # 10. Print out all the stops using a for loop
-
 for stop in stops
   p stop
 end
